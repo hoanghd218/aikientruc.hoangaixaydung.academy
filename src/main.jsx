add509336851a2cfbfe8cea55091091229c04257
Hoang218 <49197448+hoanghd218@users.noreply.github.com>
2025-12-01 08:06:24 +0700

    add ms clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { clarity } from '@microsoft/clarity'
 import './index.css'
 import App from './App.jsx'
+
+// Initialize Microsoft Clarity
+const projectId = "uegs5kjw3t"
+clarity.init(projectId)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
