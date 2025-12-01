90142b84b834e4e016d78119b1655eb821e73041
Hoang218 <49197448+hoanghd218@users.noreply.github.com>
2025-12-01 08:09:54 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { clarity } from '@microsoft/clarity'
+import clarity from '@microsoft/clarity'
 import './index.css'
 import App from './App.jsx'
 
