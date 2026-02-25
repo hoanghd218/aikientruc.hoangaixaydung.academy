afc61ac145e6f539d06a3437890f95aa07c6d82f
Hoang218 <49197448+hoanghd218@users.noreply.github.com>
2026-02-25 12:13:33 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<FreeCourse2SessionPage />} />
       <Route path="/ai-kien-truc-chuyen-sau" element={<HomePage />} />
       <Route path="/mua-khoa-hoc-ai-kientruc" element={<PaymentPageCamOn />} />
       <Route path="/ai-kien-truc-2-buoi-free" element={<FreeCourse2SessionPage />} />
